81753bb771321a19109fd01eb2e8a15a146400c1
Mushupwastaken <nolanash20082020@gmail.com>
2026-03-09 23:18:26 -0230

    Update ChunkManager.hpp

--- Code/grid/ChunkManager.hpp
+++ Code/grid/ChunkManager.hpp
@@ -8,10 +8,8 @@
 #include "GridPosition.hpp"
 #include "GridArea.hpp"
 
-#include <entt/fwd.hpp>
 
-//If we are merging registry into chunkManager, the functions would be implemented here.
-
+//Querying is fine like this, the progb
 
 namespace ne {
 
@@ -19,6 +17,10 @@ class ChunkManager {
 private:
     //Not using "GridPosition" because priv = sf::Vector2f
     std::unordered_map<sf::Vector2i, std::unique_ptr<Chunk>> m_chunks;
+
+    //For caching getChunk -> important for constant use
+    Chunk* m_lastChunkPtr = nullptr; 
+    sf::Vector2i m_lastChunkPos;
 public:
     void setChunk(Chunk chunk, GridPosition pos);
     void setTile(Tile tile, GridPosition pos);
@@ -35,38 +37,56 @@ public:
 
 //Public function API
 void ChunkManager::setChunk(Chunk chunk, GridPosition pos) {
-    auto chunkPtr = std::make_unique<Chunk>(std::move(chunk));
-    chunkPtr->updateMesh = true;
+    const auto chunkPos = pos.asChunk();
+    
+    auto [it, _] = m_chunks.insert_or_assign(chunkPos, std::make_unique<Chunk>(std::move(chunk)));
+    it->second->isDirty = true;
 
-    m_chunks.insert_or_assign(pos.asChunk(), std::move(chunkPtr));
+    m_lastChunkPtr = it->second.get();
+    m_lastChunkPos = chunkPos;
 }
 
 void ChunkManager::setTile(Tile tile, GridPosition pos) {    
     const sf::Vector2i chunkPos = pos.asChunk();
     const sf::Vector2i tilePos = pos.asTile();
     
-    auto& chunk = m_chunks[chunkPos];
+    auto& chunkPtr = m_chunks[chunkPos];
+    
+    if(!chunkPtr)
+    {
+        chunkPtr = std::make_unique<Chunk>();
+    }
 
     const sf::Vector2i localPos{ 
         (tilePos.x % constants::chunkSize + constants::chunkSize) % constants::chunkSize,
         (tilePos.y % constants::chunkSize + constants::chunkSize) % constants::chunkSize
     };
 
-    chunk->updateMesh = true;
-    chunk->tiles[Chunk::toIndex(localPos)] = tile;
+    chunkPtr->isDirty = true;
+    chunkPtr->tiles[Chunk::toIndex(localPos)] = tile;
 }
 
 inline auto* ChunkManager::getChunk(this auto&& self, GridPosition pos) {
-    if(auto it = self.m_chunks.find(pos.asChunk()); it != self.m_chunks.end())
+    const auto chunkPos = pos.asChunk();
+    
+    if(self.m_lastChunkPtr && self.m_lastChunkPos == chunkPos)
     {
-        return it->second.get();
+        return self.m_lastChunkPtr;
+    } 
+    
+    if(auto it = self.m_chunks.find(chunkPos); it != self.m_chunks.end())
+    {
+        self.m_lastChunkPtr = it->second.get();
+        self.m_lastChunkPos = chunkPos;
+        return self.m_lastChunkPtr;
     }
 
+    self.m_lastChunkPtr = nullptr; 
     return static_cast<Chunk*>(nullptr);
 }
 
 inline auto* ChunkManager::getTile(this auto&& self, GridPosition pos) {
-    if(auto* chunk = self.getChunk(pos))
+    if(auto* chunkPtr = self.getChunk(pos))
     {    
         const sf::Vector2i tilePos = pos.asTile();
         
@@ -75,10 +95,10 @@ inline auto* ChunkManager::getTile(this auto&& self, GridPosition pos) {
             (tilePos.y % constants::chunkSize + constants::chunkSize) % constants::chunkSize
         };
     
-        return &chunk->tiles[Chunk::toIndex(localPos)];
+        return &chunkPtr->tiles[Chunk::toIndex(localPos)];
     }
 
-    return nullptr;
+    return static_cast<Tile*>(nullptr);
 }
 
 template <typename Func>
@@ -90,9 +110,9 @@ void ChunkManager::queryChunks(this auto&& self, GridArea area, Func&& callback)
         for(int chunkX = chunkArea.position.x; chunkX < chunkArea.position.x + chunkArea.size.x; chunkX++)
         {
             const GridPosition currentPos = GridPosition::fromChunk({chunkX, chunkY});
-            auto* chunk = self.getChunk(currentPos);
+            auto* chunkPtr = self.getChunk(currentPos);
 
-            callback(chunk, currentPos);
+            callback(chunkPtr, currentPos);
         }
     }
 }
@@ -101,28 +121,29 @@ template <typename Func>
 void ChunkManager::queryTiles(this auto&& self, GridArea area, Func&& callback) {
     const sf::IntRect tileArea = area.asTile();
 
-    self.queryChunks(area, [&](auto* chunk, const GridPosition currentPos) {
-        if(chunk == nullptr)
+    self.queryChunks(area, [&](auto* chunkPtr, const GridPosition currentPos) {
+        if(chunkPtr == nullptr)
         {
             return;
         }
         
-        //Find tile coordinates inside chunk to parce  and callback
         const sf::Vector2i currentTilePos = currentPos.asTile();
         
         const int tileMinX = std::max(currentTilePos.x, tileArea.position.x);
         const int tileMinY = std::max(currentTilePos.y, tileArea.position.y);
-        const int tileMaxX = std::min(currentTilePos.x + constants::chunkSize, tileArea.position.x + tileArea.size.x);
-        const int tileMaxY = std::min(currentTilePos.y + constants::chunkSize, tileArea.position.y + tileArea.size.y);
+        const int tileMaxX = std::min(currentTilePos.x + constants::chunkSize - 1, tileArea.position.x + tileArea.size.x);
+        const int tileMaxY = std::min(currentTilePos.y + constants::chunkSize - 1, tileArea.position.y + tileArea.size.y);
     
-        for(int tileY = tileMinY ; tileY < tileMaxY; tileY++)
+        for(int tileY = tileMinY ; tileY <= tileMaxY; tileY++)
         {
-            for(int tileX = tileMinX; tileX < tileMaxX; tileX++)
+            for(int tileX = tileMinX; tileX <= tileMaxX; tileX++)
             {
-                const sf::Vector2i localPos{tileX - currentTilePos.x, tileY - currentTilePos.y};
-                const std::size_t index = Chunk::toIndex(localPos);
-
-                auto& tile = chunk->tiles[index];
+                const sf::Vector2i localPos{ 
+                    (tileX % constants::chunkSize + constants::chunkSize) % constants::chunkSize, 
+                    (tileY % constants::chunkSize + constants::chunkSize) % constants::chunkSize 
+                };
+                
+                auto& tile = chunkPtr->tiles[Chunk::toIndex(localPos)];
                 const GridPosition pos = GridPosition::fromTile({tileX, tileY});
 
                 callback(tile, pos);
@@ -130,6 +151,5 @@ void ChunkManager::queryTiles(this auto&& self, GridArea area, Func&& callback) 
         }
     });
 }
-
 
 } //namespace ne
